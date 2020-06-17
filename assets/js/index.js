5d60d9de1e42b9d20c4552b9c5c3defa0ef7f7a4
yang <2967157611@qq.com>
2020-06-17 21:18:52 +0800

    首页基本功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,19 +5,42 @@ $(function () {
     // 表示token不存在，跳转到登录页面
     location.href = './login.html'
   }
-  function localStorInfo(){
+  function localStorInfo() {
     $.ajax({
-      url:'http://ajax.frontend.itheima.net/my/userinfo',
-      type:'get',
+      url: 'http://ajax.frontend.itheima.net/my/userinfo',
+      type: 'get',
       // dataType:'json',
       // data:'',
-      headers:{
-        Authorization:localStorage.getItem('mytoken')
+      headers: {
+        Authorization: localStorage.getItem('mytoken')
       },
-      success: function(backData){
+      success: function (backData) {
         console.log(backData);
+        var res = backData.user_pic;
+        res = "http://t.cn/RCzsdCq";
+        if (backData.status === 0) {
+          if (res) {
+            $('.welcome').prepend('<img src="' + res + '" class="layui-nav-img">');
+            $('.welcome>div').remove();
+          };
+          var uname = backData.data.username;
+          $('.layui-nav-item>.welcome>img').after('<i>' + uname + '</i>');
+          $('.layui-side-scroll>.welcome>span').text('欢迎 ' + uname)
+        }
       }
     });
   }
   localStorInfo();
+
+  $('#logout-btn').click(function () {
+    layer.confirm('你确定退出吗？你退出了还得登录，你想好了吗?', {icon: 3, title:'提示'}, function(index){
+      //do something
+      //清除token,跳转登录页
+      localStorage.removeItem('mytoken')
+      //关闭弹窗
+      layer.close(index);
+      //跳转登录页
+      location.href = './login.html'
+    });   
+  })
 })
