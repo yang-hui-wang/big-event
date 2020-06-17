a19abb3d1b7ca875f8ad985095f48426049625f3
yang <2967157611@qq.com>
2020-06-17 12:44:15 +0800

    首页静态布局完成完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,8 @@
 $(function () {
-    // 判断token是否存在
-    var mytoken = localStorage.getItem('mytoken')
-    console.log(mytoken);
-    if (!mytoken) {
-      // 表示token不存在，跳转到登录页面
-      location.href = './login.html'
-    }
-  })
+  // 判断token是否存在
+  var mytoken = localStorage.getItem('mytoken')
+  if (!mytoken) {
+    // 表示token不存在，跳转到登录页面
+    location.href = './login.html'
+  }
+})
