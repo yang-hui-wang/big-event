9066b3e4a2cbf077e55e6ad0f1da907271d9887b
yang <2967157611@qq.com>
2020-06-17 12:34:34 +0800

    登录注册页完成

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+    // 判断token是否存在
+    var mytoken = localStorage.getItem('mytoken')
+    console.log(mytoken);
+    if (!mytoken) {
+      // 表示token不存在，跳转到登录页面
+      location.href = './login.html'
+    }
+  })
